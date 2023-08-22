7e70fa209c697a2e86449f7a3734e18169f6dccb
David Andersson <lejonmanen@gmail.com>
2023-08-22 13:43:13 +0200

    Display video stream

--- src/viewStream/ViewVideo.tsx
+++ src/viewStream/ViewVideo.tsx
@@ -1,17 +1,28 @@
-
+import { useRef } from 'react'
 
 const ViewVideo = () => {
+	const videoRef = useRef<HTMLVideoElement>(null)
+	// { current: <video> }
 
 	const handleStartStream = async () => {
+		// TypeScript tror att videoRef.current kan vara antingen null eller <video>
+		if( !videoRef.current ) return
+		const vid: HTMLVideoElement = videoRef.current
+
 		const stream: MediaStream = await navigator.mediaDevices.getUserMedia({
 			video: true, audio: false  // För att hindra rundgång i klassrummet
 		})
-
+	
+		vid.addEventListener('loadeddata', () => {
+			vid.play()
+		})
+		vid.srcObject = stream
 	}
 
 	return (
 		<div className="view-video">
 			<button onClick={handleStartStream}> Start stream </button>
+			<video controls ref={videoRef}></video>
 		</div>
 	)
 }
