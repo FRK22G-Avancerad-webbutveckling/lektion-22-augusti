a41ca59541b2febf98fef70eab124bf97fbb124d
David Andersson <lejonmanen@gmail.com>
2023-08-22 15:09:19 +0200

    Take picture

--- src/viewStream/ViewVideo.tsx
+++ src/viewStream/ViewVideo.tsx
@@ -1,8 +1,8 @@
 import { useRef } from 'react'
 
 const ViewVideo = () => {
-	const videoRef = useRef<HTMLVideoElement>(null)
-	// { current: <video> }
+	const videoRef = useRef<HTMLVideoElement>(null) // { current: <video> }
+	const imgRef = useRef<HTMLImageElement>(null)
 
 	const handleStartStream = async () => {
 		// TypeScript tror att videoRef.current kan vara antingen null eller <video>
@@ -19,10 +19,43 @@ const ViewVideo = () => {
 		vid.srcObject = stream
 	}
 
+	const handleTakePicture = async () => {
+		// 1. Starta strömmen - klart
+		// 2. Kopiera innehållet i <video> till en <canvas>
+		// - 2b. Skapa ett canvas-element
+		// - 2c. Anpassa storleken på canvas för att kopiera rätt
+		// 3. Be <canvas> om en Blob
+		// 4. Skapa en "object URL" av Blob
+		// 5. Be <img> att visa "object URL"
+
+		if( !videoRef.current ) return
+
+		const canvas: HTMLCanvasElement = document.createElement('canvas')
+		const context: CanvasRenderingContext2D | null  = canvas.getContext('2d')
+		if( !context ) return
+
+		// Ta reda på hur stort video-elementet är
+		// Canvas behöver vara lika stort
+		const w = videoRef.current.offsetWidth
+		const h = videoRef.current.offsetHeight
+		canvas.width = w
+		canvas.height = h
+
+		// Kopiera från övre vänstra hörnet på <video>
+		context.drawImage(videoRef.current, 0, 0, w, h)
+		canvas.toBlob(blob => {
+			if( !blob || !imgRef.current ) return
+			const objectURL: string = URL.createObjectURL(blob)
+			imgRef.current.src = objectURL
+		})
+	}
+
 	return (
 		<div className="view-video">
 			<button onClick={handleStartStream}> Start stream </button>
+			<button onClick={handleTakePicture}> Take picture </button>
 			<video controls ref={videoRef}></video>
+			<img ref={imgRef} />
 		</div>
 	)
 }
